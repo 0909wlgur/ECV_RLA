e05c5ef13f53eb835b8934b5b4580f1732ef7957
0909wlgur <0909wlgur@naver.com>
2025-02-07 19:35:17 +0900

    Complete | Calvin setting complete

--- scripts/setup_calvin.sh
+++ scripts/setup_calvin.sh
@@ -11,23 +11,23 @@ sudo apt-get -yqq build-dep mesa
 
 conda install -c conda-forge gcc=12.1.0 gxx_linux-64 -y
 
-git clone --recurse-submodules https://github.com/mees/calvin.git
+# git clone --recurse-submodules https://github.com/mees/calvin.git
 
 CALVIN_ROOT=$(pwd)/calvin
 cd ${CALVIN_ROOT}
-sed -i '11d' calvin_models/requirements.txt
-sed -i '12d' calvin_models/requirements.txt
-sed -i '13d' calvin_models/requirements.txt
-sed -i '14d' calvin_models/requirements.txt
+# sed -i '11d' calvin_models/requirements.txt
+# sed -i '12d' calvin_models/requirements.txt
+# sed -i '13d' calvin_models/requirements.txt
+# sed -i '14d' calvin_models/requirements.txt
 
 sh install.sh
 
 # CALVIN spesicifcally requires the following version of numpy
-pip install numpy==1.21.0
+pip install numpy==1.23.0
 
 # Download dataset
 cd ${CALVIN_ROOT}/dataset
-sh download_data.sh debug
+# sh download_data.sh debug
 # sh download_data.sh D
 # sh download_data.sh ABC
 # sh download_data.sh ABCD
