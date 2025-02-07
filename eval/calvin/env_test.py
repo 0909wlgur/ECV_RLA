74acb3709be6fc62ca92f37583ee3f9b89371706
0909wlgur <0909wlgur@naver.com>
2025-02-07 19:41:31 +0900

    Complete | Calvin setting complete

--- eval/calvin/env_test.py
+++ eval/calvin/env_test.py
@@ -1,11 +1,13 @@
 import os
 
-os.environ["PYOPENGL_PLATFORM"] = "osmesa"
+os.environ["PYOPENGL_PLATFORM"] = "egl"
 os.environ["MESA_GL_VERSION_OVERRIDE"] = "4.1"
+os.environ["EGL_DEVICE_ID"] = "2"
+
 import pyrender
 
 from calvin_env.envs.play_table_env import get_env
 
-path = "dataset/calvin_data/calvin_debug_dataset/validation"
+path = "calvin/dataset/calvin_debug_dataset/validation"
 env = get_env(path, show_gui=False)
 print(env.get_obs())
